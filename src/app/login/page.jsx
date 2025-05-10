850ea59351ddfa486e58d737245b9ee9f614d2a8
ushekate <ushekate@gmail.com>
2025-05-10 18:38:07 +0530

    update

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState } from 'react';
+import Image from 'next/image';
 // import { useRouter } from 'next/navigation';
 // import { client } from '@/utils/pocketbase';
 import "../globals.css";
@@ -35,6 +36,16 @@ const Login = () => {
 
     return (
         <div className="flex pt-45 pb-45 items-center justify-center bg-gray-800">
+            {/* <div className='flex'> */}
+            {/* <div> */}
+            <Image
+                className='image'
+                src="/girlImage.png"
+                alt="Image"
+                width={180}
+                height={38}
+            />
+            {/* </div> */}
             {/* Form for User Login */}
             <form onSubmit={(e) => { e.preventDefault(); handleLogin(email, password, role) }} className="bg-gray-300 p-8 rounded-lg shadow-md w-80">
                 <h2 className="text-2xl font-bold mb-4 text-center sm:text-2xl sm:font-bold sm:mb-4 sm:text-center">Login</h2>
@@ -74,6 +85,7 @@ const Login = () => {
                     </a>
                 </div>
             </form>
+            {/* </div> */}
         </div>
     );
 };
